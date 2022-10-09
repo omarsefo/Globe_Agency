dd011a02e7b4548c237cbbcfdf7235fb6a8b688a
unknown <omarsefo7@gmail.com>
2022-10-09 17:46:01 +0300

    update

--- g.js
+++ g.js
@@ -4,3 +4,38 @@ window.addEventListener("scroll", function () {
     nav.classList.toggle("sticky", window.scrollY > 24);
     // document.body.style.scrollBehavior="smooth";
 });
+
+var overlay = document.querySelector("[data-overlay]");
+const bee = document.getElementById("bee");
+const backmenu = document.getElementById('backmenu');
+const menup = document.getElementById('menu');
+const closep = document.getElementById('close');
+const openp = document.querySelector('.menu-btn');
+var as1 = document.getElementsByTagName('a')[2];
+var as2 = document.getElementsByTagName('a')[3];
+var as3 = document.getElementsByTagName('a')[4];
+var as4 = document.getElementsByTagName('a')[5];
+
+var navElems = [overlay, openp, closep, as1, as2, as3, as4];
+
+for (let i = 0; i < navElems.length; i++) {
+    navElems[i].addEventListener("click", function () {
+        close();
+    });
+}
+
+window.addEventListener('load', close);
+document.addEventListener('loadeddata', close);
+document.addEventListener("DOMContentLoaded", close);
+menup.addEventListener('load', close);
+openp.addEventListener('click', show);
+
+function show() {
+    menup.style.display = 'inline-block';
+    menup.style.right = '0px';
+    overlay.classList.add("over");
+}
+function close() {
+    menup.style.right = '-200%';
+    overlay.classList.remove("over");
+}
